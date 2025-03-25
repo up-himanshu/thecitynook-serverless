d68d60536c96002e99054922175c1131a6cd1861
Himanshu Upadhyaya <up.himanshu@gmail.com>
2025-03-25 16:53:34 +0530

    add Auth in allowed headers

--- src/handlers/preflight.ts
+++ src/handlers/preflight.ts
@@ -4,7 +4,7 @@ export const handler = async () => {
     // headers: {
     //   "Access-Control-Allow-Origin": "http://localhost:3000/",
     //   "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
-    //   "Access-Control-Allow-Headers": "Content-Type",
+    //   "Access-Control-Allow-Headers": "Content-Type, Authorization, key",
     // },
     body: null,
   };
